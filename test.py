16c89f6d740b753528e3682d31660589c4212820
hamanon <physicsnoopy.k.o@gmail.com>
2019-01-19 17:25:52 +0900

    最短の距離と場所を特定

--- test.py
+++ test.py
@@ -2,21 +2,25 @@
 import osmnx as ox
 import networkx as nx
 import matplotlib.pyplot as plt
+from shapely.geometry import Point,LineString
 
 #%%
 G = ox.graph_from_bbox(north=35.7556,south=35.7495,west=139.7441,east=139.7531,network_type="drive")
 G_projected = ox.project_graph(G, to_crs={'init': 'epsg:4301'})
-fig, ax = ox.plot_graph(G_projected, show=False, close=False)
-ax.scatter(139.749536,35.746767, c='red')
-#%%
-basic_stats = ox.basic_stats(G)
-print(basic_stats)
-
-#%%
 edges = ox.graph_to_gdfs(G_projected, nodes=False, edges=True)
-edges.head()
-#%%
+distance=edges["geometry"][0].project(point)
+point_onRoad=edges["geometry"][0].interpolate(distance).coords[0]
+
+fig, ax = ox.plot_graph(G_projected, show=False, close=False)
+point_x=139.749536
+point_y=35.746767
+ax.scatter(point_x, point_y, c='red')
+point=Point(point_x,point_y)
+i=1
+distance=edges["geometry"][i].project(point)
+point_onRoad=edges["geometry"][i].interpolate(distance).coords[0]
+ax.scatter(edges["geometry"][i].coords[:][0][0],edges["geometry"][i].coords[:][0][1],c='pink')
+ax.scatter(edges["geometry"][i].coords[:][-1][0],edges["geometry"][i].coords[:][-1][1],c='green')
+ax.scatter(point_onRoad[0],point_onRoad[1],c='blue')
 
 
-
-#%%
